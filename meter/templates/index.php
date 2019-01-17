87a5cc78766dcdfd9874fa6068a5c0887544c378
denigud <denigud@ya.ru>
2019-01-18 00:20:33 +0200

    buttons

--- meter/templates/index.php
+++ meter/templates/index.php
@@ -12,7 +12,7 @@
             <?php $meter = $value->getData();?>
             <div class="card text-white bg-<?php echo $meter['cardStyle']?> mb-3">
                 <div class="card-header"><a href="<?php echo $meter['site']?>"><?php echo $meter['title'] ?></a>
-                    <img src="<?php echo $meter['image'] ?>" class="card-img-top" alt="...">
+                    <img src="<?php echo $meter['image'] ?>" class="card-img-top" alt="logo">
                 </div>
 
                 <div class="card-body">
@@ -21,6 +21,7 @@
                         <tr>
                             <th scope="col">Дата</th>
                             <th scope="col">Показание</th>
+                            <th></th>
                         </tr>
                         </thead>
                         <tbody>
@@ -29,6 +30,16 @@
                             <tr>
                                 <th scope="row"><?php echo $meterReading['date']?></th>
                                 <td><?php echo $meterReading['reading']?></td>
+                                <td>
+                                    <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
+                                        <button type="button" class="btn btn-primary btn-sm"><span>
+                                            <span><img src="/meter/ico/pen_1.png" alt="Edit"></span>
+                                        </button>
+                                        <button type="button" class="btn btn-secondary btn-sm">
+                                            <span><img src="/meter/ico/circle_delete.png" alt="Delete"></span>
+                                        </button>
+                                    </div>
+                                </td>
                             </tr>
                             <?php endif;?>
                         <?php endforeach;?>
@@ -38,7 +49,22 @@
                     <p class="card-text"></p>
                 </div>
                 <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
+                    <form>
+                        <div class="row">
+                            <div class="col">
+                                <input type="date" class="form-control" placeholder="Date">
+                            </div>
+                            <div class="col">
+                                <input type="text" class="form-control" placeholder="Reading">
+                            </div>
+                            <div class="col">
+                                <button type="button" class="btn btn-success">
+                                    <span><img src="/meter/ico/circle_plus.png" alt="Add"></span>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                    <!--<small class="text-muted">Last updated 3 mins ago</small>-->
                 </div>
             </div>
         <?php endforeach;?>
